a5f9e92cb6e4f7850b2f49d164d149f2982766fa
Sooyong Lee <sooyongl09@utexas.edu>
2026-06-06 15:20:37 -0500

    Update site

---
+++ prerun/pkgdown.R
@@ -0,0 +1,12 @@
+# 1. Prepare package documentation
+devtools::document()
+
+# # 2. Check package
+# devtools::check()
+#
+# # 3. Create pkgdown config
+# usethis::use_pkgdown()
+# usethis::use_vignette("getting-started")
+
+# 4. Build locally
+pkgdown::build_site()
